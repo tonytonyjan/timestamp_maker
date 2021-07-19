b767d9a877eedd15912fb6a2e45f29495e423010
Jian Weihang <tonytonyjan@gmail.com>
2021-07-19 09:54:16 +0800

    feat: support specifying custom time

--- lib/timestamp_maker.rb
+++ lib/timestamp_maker.rb
@@ -14,7 +14,7 @@ module TimestampMaker
   class << self
     attr_reader :mime_recognizer, :image_timestamper, :video_timestamper
 
-    def add_timestamp(input_path, output_path, format: '%Y-%m-%d %H:%M:%S')
+    def add_timestamp(input_path, output_path, format: '%Y-%m-%d %H:%M:%S', time: nil)
       mime_type = mime_recognizer.recognize(input_path)
       processor =
         case mime_type.split('/').first
@@ -22,7 +22,9 @@ module TimestampMaker
         when 'video' then video_timestamper
         else raise "Unsupported MIME type: ##{mime_type}"
         end
-      time = processor.creation_time(input_path)
+      time = processor.creation_time(input_path) if time.nil?
+      raise ArgumentError unless time.is_a?(Time)
+
       processor.add_timestamp(input_path, output_path, time, format: format)
     end
   end
